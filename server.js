e0c4e56a2d3fc4522acb58a18d7620169870fce6
SAMUEL GIRARDIN <aaxyz@hotmail.fr>
2025-03-17 13:25:25 +0100

    serve noaa

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import axios from 'axios';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+// Pour obtenir __dirname dans les modules ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the 'downloads' folder
+app.use('/downloads', express.static(path.join(__dirname, 'downloads')));
+
+app.get('/api/grib-data', async (req, res) => {
+  const filename = req.query.filename;
+  const date = req.query.date;
+  const local = req.query.local === 'true';
+
+  let url;
+
+  if (!local) {
+    url = `https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25_1hr.pl?dir=%2Fgfs.${date}%2F00%2Fatmos&file=gfs.t00z.pgrb2.0p25.f${filename}&var_UGRD=on&var_VGRD=on&lev_10_m_above_ground=on`;
+  } else {
+    url = `http://localhost:3000/downloads/gfs.t00z.pgrb2.0p25.f${filename}`;
+  }
+
+  console.log('get', url);
+
+  try {
+    const response = await axios({
+      method: 'get',
+      url: url,
+      responseType: 'arraybuffer',
+    });
+
+    if (!local) {
+      const filePath = path.join(__dirname, 'downloads', `gfs.t00z.pgrb2.0p25.f${filename}`);
+      fs.writeFileSync(filePath, response.data);
+    }
+
+    res.set('Content-Type', 'application/octet-stream');
+    res.send(response.data);
+  } catch (error) {
+    console.error('server error', filename, 'error download');
+    res.status(500).send('Erreur lors de la récupération des données');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Serveur démarré sur le port 3000');
+});
